e69cddb3f1af6dd06d722baa76b4c7577a9846ec
MUKILTEO\1726393 <henryclare@outlook.com>
2026-03-19 12:33:44 -0700

    Start Menu Done!

--- Assets/People/Henry/Start menu.cs
+++ Assets/People/Henry/Start menu.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Startmenu : MonoBehaviour
 {
+    [SerializeField] private float swapTime = 5f;
+    [SerializeField] private Sprite[] backgrounds;
+
+    [SerializeField] private Image currentImage;
     public void StartButton()
     {
         SceneManager.LoadScene("Main");
@@ -12,5 +19,20 @@ public class Startmenu : MonoBehaviour
     {
         Debug.Log("Quit the game!");
         Application.Quit(); 
-    } 
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Scroll());
+    }
+
+    IEnumerator Scroll()
+    {
+        foreach(Sprite spr in backgrounds)
+        {
+            currentImage.sprite = spr;
+            yield return new WaitForSeconds(swapTime);
+        }
+        StartCoroutine(Scroll());
+    }
 }
