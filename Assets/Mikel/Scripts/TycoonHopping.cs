9d0ec563eedee9f2bf3f93da1c8bb5f4a856968c
MUKILTEO\1726377 <chasebannavong@gmail.com>
2026-04-28 11:24:38 -0700

    ya

--- Assets/Mikel/Scripts/TycoonHopping.cs
+++ Assets/Mikel/Scripts/TycoonHopping.cs
@@ -7,12 +7,61 @@ public class TycoonHopping : MonoBehaviour
 {
     public void TycoonHop()
     {
-        SceneManager.LoadScene("Tycoon");
+        SceneManager.LoadScene("GoldmanWieners");
     }
 
     public void TycoonHop2()
     {
-        SceneManager.LoadScene("Tycoon2");
+        SceneManager.LoadScene("LardDonuts");
+    }
+
+    public void TycoonHop3()
+    {
+        SceneManager.LoadScene("CuttinCorral");
+    }
+
+    public void TycoonHop4()
+    {
+        SceneManager.LoadScene("LuigisPizzaria");
+    }
+
+    public void TycoonHop5()
+    {
+        SceneManager.LoadScene("JoesToilets");
+    }
+
+    public void TycoonHop6()
+    {
+        SceneManager.LoadScene("StatueOfLiburty");
+    }
+
+    public void TycoonHop7()
+    {
+        SceneManager.LoadScene("ChudsOil");
+    }
+
+    public void TycoonHop8()
+    {
+        SceneManager.LoadScene("Cinima");
+    }
+
+    public void TycoonHop9()
+    {
+        SceneManager.LoadScene("Bank");
+    }
+
+    public void TycoonHop10()
+    {
+        SceneManager.LoadScene("YankeeStadium");
+    }
+    public void TycoonHop11()
+    {
+        SceneManager.LoadScene("StPatricksCatholicChurch");
+    }
+
+    public void TycoonHop12()
+    {
+        SceneManager.LoadScene("EmpireStateBuilding");
     }
 
     public void MapHop()
