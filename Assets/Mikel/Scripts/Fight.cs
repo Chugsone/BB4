52b920bbe45b2d21a3bf5f4f975e12dec8b0f687
DESKTOP-2Q7I9E8\mbann <chasebannavong@gmail.com>
2026-05-05 20:34:32 -0700

    fight

---
+++ Assets/Mikel/Scripts/Fight.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class Fight : MonoBehaviour
+{
+    public void FightScene()
+    {
+        SceneManager.LoadScene("Rian");
+    
+    }
+}
